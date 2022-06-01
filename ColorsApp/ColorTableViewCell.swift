4bf4ec33ace03f1b7f65817f15c93ff943cfadac
OsherYe <osheryeffet137@gmail.com>
2022-06-01 11:43:40 +0300

    storyboard tableViewCell

---
+++ ColorsApp/ColorTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ColorTableViewCell.swift
+//  ColorsApp
+//
+//  Created by Osher Yeffet on 01/06/2022.
+//
+
+import UIKit
+
+class ColorTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ColorsApp/SingleColorViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SingleColorViewController.swift
+//  ColorsApp
+//
+//  Created by Osher Yeffet on 01/06/2022.
+//
+
+import UIKit
+
+class SingleColorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
